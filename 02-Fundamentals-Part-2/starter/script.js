ae5aec65cf9c31d47408c12e4ce4889108d88952
YOOUMUU <yooumuu@gmail.com>
2022-09-23 02:02:03 +0800

    finish 2.1

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,27 @@
+"use strict";
+/*
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log("I can drive");
+*/
+/*
+function logger() {
+  console.log("My name is Jonas");
+}
+
+// calling / running / invoking function
+logger();
+logger();
+logger();
+
+function fruitProcesser(apples, oranges) {
+  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+  return juice;
+}
+
+const applejuice = fruitProcesser(5, 0);
+console.log(applejuice);
+console.log(fruitProcesser(5, 0));
+*/
