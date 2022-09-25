36e5e5cdade9ce6248c41e5b85b4791bf6c2b564
YOOUMUU <yooumuu@gmail.com>
2022-09-25 09:48:07 +0800

    2.14 commit

--- 02-Fundamentals-Part-2/starter/assignment.js
+++ 02-Fundamentals-Part-2/starter/assignment.js
@@ -176,3 +176,45 @@ myCountry.describe();
 myCountry.checkIsland();
 console.log(myCountry.isIsland);
 */
+
+/*
+const mark = {
+  firstName: "Mark",
+  lastName: "Miller",
+  mass: 78,
+  height: 1.69,
+  calcBMI: function () {
+    this.bmi = this.mass / this.height ** 2;
+    return this.bmi;
+  },
+};
+
+const john = {
+  firstName: "John",
+  lastName: "Smith",
+  mass: 92,
+  height: 1.95,
+  calcBMI: function () {
+    this.bmi = this.mass / this.height ** 2;
+    return this.bmi;
+  },
+};
+
+if (mark.calcBMI() > john.calcBMI()) {
+  console.log(
+    `${mark.firstName} ${
+      mark.lastName
+    }'s BMI (${mark.calcBMI()}) is higher than ${john.firstName} ${
+      john.lastName
+    }'s (${john.calcBMI()})!`
+  );
+} else if (mark.calcBMI() < john.calcBMI()) {
+  console.log(
+    `${john.firstName} ${
+      john.lastName
+    }'s BMI (${john.calcBMI()}) is higher than ${mark.firstName} ${
+      mark.lastName
+    }'s (${mark.calcBMI()})!`
+  );
+}
+*/
