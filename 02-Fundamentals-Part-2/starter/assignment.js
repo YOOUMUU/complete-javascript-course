dfa46baec24cfd328b4064ec538ed017caf05c4b
YOOUMUU <yooumuu@gmail.com>
2022-09-25 10:25:16 +0800

    2.15 commit

--- 02-Fundamentals-Part-2/starter/assignment.js
+++ 02-Fundamentals-Part-2/starter/assignment.js
@@ -218,3 +218,9 @@ if (mark.calcBMI() > john.calcBMI()) {
   );
 }
 */
+
+/*
+for (let voter = 1; voter <= 50; voter++) {
+  console.log(`Voter number ${voter} is currently voting`);
+}
+*/
--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -274,3 +274,10 @@ console.log(jonas.age);
 
 console.log(jonas.getSummary());
 */
+
+/*
+// for loop keeps running while condition is TRUE
+for (let rep = 1; rep <= 10; rep++) {
+  console.log(`Lifting weights repetition ${rep} 🏋️‍♀️`);
+}
+*/
